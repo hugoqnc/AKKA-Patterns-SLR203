a6f96ad370c443287eb15c30d87d276a5e493997
Hugo Queinnec <hugoqueinnec@MacBook-Pro-de-Hugo.local>
2021-01-17 11:31:06 +0100

    add 20th implementation

---
+++ 20UncontrolledFlooding/src/main/java/demo/UncontrolledFlooding.java
@@ -0,0 +1,52 @@
+package demo;
+
+import java.util.ArrayList;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+/**
+ * @author Remi SHARROCK and Axel Mathieu
+ * @description Create an actor and passing his reference to
+ *				another actor by message.
+ */
+public class UncontrolledFlooding {
+
+	public static void main(String[] args) {
+		// Instantiate an actor system
+		final ActorSystem system = ActorSystem.create("system");
+		
+		// Instantiate actors
+		final ActorRef a = system.actorOf(BasicActor.createActor(), "a");
+		final ActorRef b = system.actorOf(BasicActor.createActor(), "b");
+		final ActorRef c = system.actorOf(BasicActor.createActor(), "c");
+		final ActorRef d = system.actorOf(BasicActor.createActor(), "d");
+		final ActorRef e = system.actorOf(BasicActor.createActor(), "e");
+
+	    final ActorRef manager = system.actorOf(ManagerActor.createActor(), "manager");
+		
+		ArrayList<ActorRef> list = new ArrayList<ActorRef>();
+		list.add(a);
+		list.add(b);
+		list.add(c);
+		list.add(d);
+		list.add(e);
+		manager.tell(new MessageListOfRef(list), ActorRef.noSender());
+	    
+	
+	    // We wait 5 seconds before ending system (by default)
+	    // But this is not the best solution.
+	    try {
+			waitBeforeTerminate();
+		} catch (InterruptedException error) {
+			error.printStackTrace();
+		} finally {
+			system.terminate();
+		}
+	}
+
+	public static void waitBeforeTerminate() throws InterruptedException {
+		Thread.sleep(5000);
+	}
+
+}
---
+++ 21ControlledFlooding/src/main/java/demo/ManagerActor.java
@@ -0,0 +1,83 @@
+package demo;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedAbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class ManagerActor extends UntypedAbstractActor {
+
+	private ArrayList<ActorRef> listOfActorRef;
+	private HashMap<Integer, ArrayList<Integer>> matrix = new HashMap<Integer, ArrayList<Integer>>();
+
+	private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+	// Empty Constructor
+	public ManagerActor() {}
+
+	// Static function that creates actor
+	public static Props createActor() {
+		return Props.create(ManagerActor.class, () -> {
+			return new ManagerActor();
+		});
+	}
+
+	private void createMatrix(){
+		matrix.put(1, new ArrayList<Integer>(Arrays.asList(2,3)));
+		matrix.put(2, new ArrayList<Integer>(Arrays.asList(4)));
+		matrix.put(3, new ArrayList<Integer>(Arrays.asList(4)));
+		matrix.put(4, new ArrayList<Integer>(Arrays.asList(5)));
+		matrix.put(5, new ArrayList<Integer>());
+		//matrix.put(5, new ArrayList<Integer>(Arrays.asList(2))); //infinite loop
+	}
+
+	private void sendRef(){
+		for(int i = 1; i<=listOfActorRef.size(); i++){
+
+			ArrayList<ActorRef> actorsToSend = new ArrayList<ActorRef>();
+
+			for (int j : matrix.get(i)){
+				actorsToSend.add(listOfActorRef.get(j-1));
+			}
+			listOfActorRef.get(i-1).tell(new MessageListOfRef(actorsToSend), getSelf());
+		}
+	}
+
+	@Override
+	public void onReceive(Object message) throws Throwable {
+
+		if (message instanceof MessageListOfRef){
+			listOfActorRef = ((MessageListOfRef)message).list;
+
+			String names = "";
+			for (ActorRef actor : listOfActorRef){
+				names = names.concat(actor.path().name()+",");
+			}
+			names = names.substring(0, names.length()-1);
+
+			log.info("["+getSelf().path().name()+"] received message with the list of Ref '" + names + "' from ["+ getSender().path().name() +"]");
+
+
+			createMatrix();
+			sendRef();
+
+			getContext().system().scheduler().scheduleOnce(Duration.ofMillis(1000), getSelf(), "go", getContext().system().dispatcher(), ActorRef.noSender());
+		}
+		else if (message instanceof String) {
+			String data = (String)message;
+			if (data=="go"){
+				String msg = "Packet";
+				listOfActorRef.get(0).tell(msg, getSelf());
+			}
+		}
+
+	}
+	
+	
+}
